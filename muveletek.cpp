eaffc9744a2e9dd855aa5e4631a6fc122041dff5
SerLeopold <79343488+SerLeopold@users.noreply.github.com>
2023-01-24 09:09:24 +0100

    Add files via upload

---
+++ muveletek.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    float a,b;
+    cout << "Adj meg egy szamot: ";
+    cin >>a;
+    cout << "Adj meg egy masik szamot: ";
+    cin >> b;
+
+    cout << "A ket szam osszege: " << a+b << endl;
+    cout << "A ket szam kulonbsege: " << a-b << endl;
+    cout << "A ket szam szorzata: " << a*b << endl;
+    cout << "A ket szam hanyadosa: " << a/b << endl;
+    //cout << "A ket szam osztasi maradeka: " << a%b << endl;
+
+    return 0;
+
+
+}
+
